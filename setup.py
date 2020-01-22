52544f721cbd198aa95dcf1c6b382871bc6d0907
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-01-22 14:13:49 +0800

    修理代碼語法不能沒有一行的問題

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('readme.md', encoding='utf8') as fh:
     
 setuptools.setup(
     name='liber',
-    version='0.0.1',
+    version='0.0.2',
     author='RimoChan',
     author_email='the@librian.it',
     description='A library to compile liber.',
